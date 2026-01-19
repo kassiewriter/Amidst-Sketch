4fe763011dd67d18395f7003d125aff83c5ac4e4
Kassie N <julianl@Julians-MacBook-Air-2.local>
2026-01-19 17:07:24 -0500

    Quartz sync: Jan 19, 2026, 5:07 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,25 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'kassiewriter/Amidst-Sketch',
+        // from data-repo-id
+        repoId: 'R_kgDOQ9EYBQ',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDOQ9EYBc4C1KVL',
+        // from data-lang
+        lang: 'en',
+        lightTheme: 'https://giscus.catppuccin.com/themes/latte.css',
+        darkTheme: 'https://giscus.catppuccin.com/themes/mocha.css'
+      }
+   }),
+],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
