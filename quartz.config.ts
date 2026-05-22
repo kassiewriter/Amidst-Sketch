ba716758822a46b8b3610c19c3c39bd2b45bbf53
Kassie N <julianl@Julians-MacBook-Air-2.local>
2026-05-21 22:06:58 -0400

    Quartz sync: May 21, 2026, 10:06 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Rainbow Dilemma",
+    pageTitle: "Bodysnatchers TYoS",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
